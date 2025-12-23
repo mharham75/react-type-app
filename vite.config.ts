266690328f81294a0a01df1887f9d94bc4ac3d91
Md Arham <mharham75@gmail.com>
2025-12-23 23:32:20 +0530

    added vitest and rtl and updated configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -12,5 +12,11 @@ export default defineConfig({
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@services': path.resolve(__dirname, 'src/services'),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
   },
 });
